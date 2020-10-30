80ccc43fbf729e7f135eab84d1f95163d20e6fb8
Alan Barton <abarton201@gmail.com>
2020-10-29 22:24:18 -0400

    made index action

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="home">
-    <h1>{{ message }}</h1>
+    <h1>Users</h1>
+    <div v-for="user in users">
+      <p>{{ user.name }}</p>
+    </div>
   </div>
 </template>
 
@@ -8,13 +11,24 @@
 </style>
 
 <script>
+import axios from "axios";
+
 export default {
   data: function() {
     return {
-      message: "Welcome to Vue.js!"
+      users: []
     };
   },
-  created: function() {},
-  methods: {}
+  created: function() {
+    this.indexUsers();
+  },
+  methods: {
+    indexUsers:function() {
+      axios.get("/api/users").then(response => {
+        console.log("Users index", response);
+        this.users = response.data;
+      });
+    }
+  }
 };
 </script>
