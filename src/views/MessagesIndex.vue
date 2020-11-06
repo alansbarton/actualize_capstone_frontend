c85f13518408206ecd6cc4181654d90ebff2d41f
Alan Barton <abarton201@gmail.com>
2020-11-05 22:24:14 -0500

    fixing things

--- src/views/MessagesIndex.vue
+++ src/views/MessagesIndex.vue
@@ -7,8 +7,8 @@
     </div>
     <h1>All Messages</h1>
     <div v-for="message in messages">
-      <p><strong>{{ message.sender_id }}</strong> : {{ message.created_at }}</p>
-      <p>{{ message.message }}</p>
+      <p><strong>{{ message.name }}</strong> : {{ message.created_at }}</p>
+      <p>{{ message.body }}</p>
     </div>
   </div>
 </template>
@@ -24,10 +24,11 @@ export default {
   data: function() {
     return {
       messages: [],
+      newMessageBody: "",
     };
   },
   created: function() {
-    this.indexPhotos();
+    this.createMessage();
 
     var cable = ActionCable.createConsumer("ws://localhost:3000/cable");
 
@@ -48,10 +49,13 @@ export default {
   },
 
   methods: {
-    indexPhotos: function() {
-      axios.get("/api/messages").then(response => {
+    createMessage: function() {
+      var params = {
+        body: this.newMessageBody
+      };
+      axios.get("/api/messages", params).then(response => {
         console.log("messages index", response);
-        this.photos = response.data;
+        this.newMessageBody = "";
       });
     },
   },
